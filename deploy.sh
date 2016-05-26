c811be602ff7591b4dcbb97be251698e4fe182e2
Oliver Steensen-Bech Haagh <osh@dma.dk>
2016-05-26 15:30:25 +0200

    containers now always starts on system startup except if they have been stopped before manually

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,11 @@ docker pull mysql
 docker network create baltic 
 
 #create the containers and link them
-docker create --name db --net=baltic -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo mysql
+docker create --name db --net=baltic --restart=unless-stopped -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo mysql
 
-docker create --name couch --net=baltic -v $HOME/balticweb/couchdb:/data dmadk/embryo-couchdb
+docker create --name couch --net=baltic --restart=unless-stopped -v $HOME/balticweb/couchdb:/data dmadk/embryo-couchdb
 
-docker create --name balticweb --net=baltic -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
+docker create --name balticweb --net=baltic --restart=unless-stopped -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
 
 # start all containers
 docker start db couch balticweb
