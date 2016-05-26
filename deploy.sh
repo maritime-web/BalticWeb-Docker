2245a230ca92d452948d0b5f8d836b69e18a336e
Oliver Steensen-Bech Haagh <osh@dma.dk>
2016-05-26 13:54:57 +0200

    went away from using internal network, now using a simple bridge network instead

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,15 @@ docker pull dmadk/balticweb
 docker pull dmadk/embryo-couchdb
 docker pull mysql
 
-#create an internal network called baltic
-docker network create --internal baltic 
+#create a network called baltic
+docker network create baltic 
 
 #create the containers and link them
 docker create --name db --net=baltic -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo mysql
 
 docker create --name couch --net=baltic -v $HOME/balticweb/couchdb:/data dmadk/embryo-couchdb
 
-docker create --name balticweb --net=baltic --link db:db --link couch:couch -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
-
-#connect the balticweb container to the default docker bridge
-docker network connect bridge balticweb
+docker create --name balticweb --net=baltic -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
 
 # start all containers
 docker start db couch balticweb
