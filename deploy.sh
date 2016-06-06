4a21a7921f5d9197073e048b30c864b106fefb44
Oliver Steensen-Bech Haagh <osh@dma.dk>
2016-06-06 15:18:23 +0200

    started using the efk stack for logging

--- deploy.sh
+++ deploy.sh
@@ -13,17 +13,17 @@ full () {
 
     #create a network called baltic
     echo "Creating network"
-    docker network create baltic 
+    docker network create baltic
 
     #create the containers and link them
     echo "Creating containers"
-    docker create --name db --net=baltic --restart=unless-stopped -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo mysql
+    docker create --name db --net=baltic --log-driver=fluentd --restart=unless-stopped -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo mysql
 
-    docker create --name couch --net=baltic --restart=unless-stopped -v $HOME/balticweb/couchdb:/data dmadk/embryo-couchdb
+    docker create --name couch --net=baltic --log-driver=fluentd --restart=unless-stopped -v $HOME/balticweb/couchdb:/data dmadk/embryo-couchdb
 
-    docker create --name balticweb --net=baltic --restart=unless-stopped -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
+    docker create --name balticweb --net=baltic --log-driver=fluentd --restart=unless-stopped -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
 
-    docker create --name watchtower --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock centurylink/watchtower balticweb
+    docker create --name watchtower --log-driver=fluentd --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock centurylink/watchtower balticweb
 }
 
 $1
