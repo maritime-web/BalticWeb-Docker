d3f1d40dee1af5f1c70e5ca750c0d76d60841363
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-06-21 14:31:27 +0200

    Adding deployment startscript fit for local development

---
+++ deploy_local.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+#script to create and deploy all containers needed for deployment of balticweb
+
+#pull images and create network and containers
+full () {
+    #pull the latest images
+    echo "Pulling latest images"
+    docker pull dmadk/balticweb
+    docker pull dmadk/embryo-couchdb
+    docker pull mysql
+   # docker pull centurylink/watchtower
+
+    #create a network called baltic
+    echo "Creating network"
+    docker network create baltic
+
+    #create the containers and link them
+    echo "Creating containers"
+    docker create --name db --net=baltic --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -v $HOME/balticweb/mysql/datadir:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo mysql
+
+    docker create --name couch --net=baltic --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -v $HOME/balticweb/couchdb:/data dmadk/embryo-couchdb
+
+    docker create --name balticweb --net=baltic --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
+
+    #docker create --name watchtower --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock centurylink/watchtower balticweb --cleanup
+}
+
+$1
+
+# start all containers
+echo "Starting containers"
+docker start db couch balticweb 
+# watchtower
+
+exit 0
