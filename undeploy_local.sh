d85f5ca190673d7cc081c706289caeae4d9358af
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-06-21 15:04:42 +0200

    added local undeploy file

---
+++ undeploy_local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Stopping containers"
+docker stop balticweb db couch
+
+full ()
+{
+    echo "Removing containers"
+    docker rm balticweb db couch
+    docker network rm baltic
+}
+
+$1
+
+exit 0
