49939eaa432f44e6c96c1af0aa6db683fa505ffc
Andreas Borchsenius Westh <andreas@lundogbendsen.dk>
2016-06-27 09:39:18 +0200

    adding deploy scripts for local development

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+echo "┌┬┐┌─┐┌┐┌┬┌─┐┬ ┬  ┌┬┐┌─┐┬─┐┬┌┬┐┬┌┬┐┌─┐  ┌─┐┬ ┬┌┬┐┬ ┬┌─┐┬─┐┬┌┬┐┬ ┬";
+echo " ││├─┤││││└─┐├─┤  │││├─┤├┬┘│ │ ││││├┤   ├─┤│ │ │ ├─┤│ │├┬┘│ │ └┬┘";
+echo "─┴┘┴ ┴┘└┘┴└─┘┴ ┴  ┴ ┴┴ ┴┴└─┴ ┴ ┴┴ ┴└─┘  ┴ ┴└─┘ ┴ ┴ ┴└─┘┴└─┴ ┴  ┴ ";
 #script to create and deploy all containers needed for deployment of balticweb
 
 #pull images and create network and containers
@@ -17,11 +19,12 @@ full () {
 
     #create the containers and link them
     echo "Creating containers"
-    docker create --name db --net=baltic --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -v $HOME/balticweb/mysql/datadir:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo -p 3306:3306 mysql
+     # --log-opt fluentd-async-connect=true
+    docker create --name db --net=baltic --log-driver=fluentd --restart=unless-stopped -v $HOME/balticweb/mysql/datadir:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=embryo -e MYSQL_PASSWORD=embryo -e MYSQL_DATABASE=embryo -p 3306:3306 mysql
 
-    docker create --name couch --net=baltic --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -v $HOME/balticweb/couchdb:/data  -p 5984:5984 dmadk/embryo-couchdb
+    docker create --name couch --net=baltic --log-driver=fluentd --restart=unless-stopped -v $HOME/balticweb/couchdb:/data  -p 5984:5984 dmadk/embryo-couchdb
 
-    docker create --name balticweb --net=baltic --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -p 8080:8080 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
+    docker create --name balticweb --net=baltic --log-driver=fluentd --restart=unless-stopped -p 8080:8080 -p 9990:9990 -v $HOME/balticweb/properties:/opt/jboss/wildfly/balticweb_properties dmadk/balticweb
 
     #docker create --name watchtower --log-driver=fluentd --log-opt fluentd-async-connect=true --restart=unless-stopped -v /var/run/docker.sock:/var/run/docker.sock centurylink/watchtower balticweb --cleanup
 }
@@ -29,12 +32,24 @@ full () {
 $1
 
 # start logging
-echo "Stadorting logging"
+echo "Starting logging"
 docker-compose -f logging/docker-compose.yml up -d
 
 # start all containers
 echo "Starting containers"
 docker start db couch balticweb
-# watchtower
+
+# the web administrative DOCKER UI     
+docker rm mgmt-Docker-UI              
+docker run --name mgmt-Docker-UI -d -t -p 9999:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock uifd/ui-for-docker
+
+echo "# watchtower - disabled";
+echo "# the web application (WILDFLY)                     http://localhost:8080";
+echo "# the web administrative application (WILDFLY)      http://localhost:9990";
+echo "# the web administrative DOCKER UI                  http://localhost:9999";
+echo "# Couch DB                                          http://localhost:5984/_utils/index.html";
+echo "# MYSQL localhost:3306 use desktop client like mysql workbench";
+echo "# Logging user interface Kibana                     http://localhost:5601/app/kibana#/";
+echo "# fluentd https://github.com/fluent/fluentd Fluentd: Open-Source Log Collector";
 
 exit 0
